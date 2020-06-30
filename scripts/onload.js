1949faf6b83648e5b831607ba2581c3ca2c36a5f
ReneStalitza <Renestalitza@web.de>
2020-06-30 14:58:39 +0200

    Add files via upload

--- scripts/onload.js
+++ scripts/onload.js
@@ -4,10 +4,12 @@
  */
 var places;
 // Set max/min coordinates to interact in a smaller area TODO: change to var values
-var maxLat = 51.965894;
+var maxLat = 51.968956;
 var minLat = 51.962976;
-var maxLon = 7.635459;
-var minLon = 7.633912;
+var maxLon = 7.636953;
+var minLon = 7.620886;
+
+var active;
 
 window.onload = () => {
     fetch('https://raw.githubusercontent.com/snavas/InteractionWithGeoinformation/master/assets/osmtrees.geojson')
@@ -22,12 +24,14 @@ window.onload = () => {
             const species = place.species;
             // just add trees of selected area
             if (latitude < maxLat && latitude > minLat && longitude < maxLon && longitude > minLon) {
-
+                var array = [];
+                
                 const icon = document.createElement('a-text');
                 icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
                 icon.setAttribute('look-at', '[gps-camera]');
-                icon.setAttribute('scale', '4 4 4');
-                icon.setAttribute('value', 'X');
+                icon.setAttribute('clickable','');
+                icon.setAttribute('description', '');
+                icon.setAttribute('scale', '7 7 7');
                 icon.setAttribute('geometry', 'primitive: ring; radiusInner: 0.11; radiusOuter: 0.14');
                 icon.setAttribute('align', 'center');
                 if (species == "" || species == undefined) {
@@ -35,11 +39,109 @@ window.onload = () => {
                 } else {
                     icon.setAttribute('species', species);
                 }
-                console.log(icon.getAttribute('species'));
+               // console.log(icon.getAttribute('species'));
                 checkIfEndangered(icon);
                 scene.appendChild(icon);
             } else {};
         });
         distanceMsg = document.querySelector('[gps-entity-place]').getAttribute('distance');
-    }, 1000);
+    }, 3000);
 };
+
+ AFRAME.registerComponent('clickable', {
+            init: function(){
+                let el = this.el;
+                
+                // set color on click dependent on index component
+                el.addEventListener('click', e =>{ 
+                
+                el.setAttribute('geometry', 'radiusOuter: 0.2');
+                if(active == "" || active == undefined) {}
+                else{
+                     active.setAttribute('geometry', 'radiusOuter: 0.14' );
+                };
+                    
+                active = el; 
+                 document.getElementById('treeSpecies').innerHTML= el.getAttribute('species');
+                    
+                if(el.components.text.data.color == 'orange' || el.components.text.color == 'red' ){        document.getElementById('pot').innerHTML= 'Yes';
+                }
+                else {
+                 document.getElementById('pot').innerHTML= 'No';
+                 }
+                   
+                document.getElementById("myList").innerHTML='';
+                let len = el.components.description.data.length;
+                   if(len > 0){ 
+                       for(var i =0; i < len; i++ ){
+                           document.getElementById("myList").appendChild(el.components.description.data[i]);
+                    }
+                   }
+                    else {
+                        document.getElementById("myList").innerHTML='';
+                    }
+            
+                });
+             }
+      });
+
+AFRAME.registerComponent('description', {
+    schema: {type: 'array', default:[]}
+      });
+
+
+function openInfobox() {
+    let infobox = document.querySelector("#infobox");
+    infobox.classList.toggle("opened");
+    let button = document.querySelector(".button-in");
+    button.classList.toggle("open");
+}
+
+function addInfo() {
+  if (active == "" || active == undefined){
+      alert("Please select a tree");
+  }
+  else { 
+  var node = document.createElement("LI");
+  var text = document.getElementById('des').value;
+  var textnode = document.createTextNode(text);
+  node.appendChild(textnode);
+  document.getElementById("myList").appendChild(node);
+  
+  active.components.description.data.push(node);
+    
+  document.getElementById('des').value = "";
+  }
+}
+/*
+function change(){
+    
+}
+*/
+
+/*
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log('Your current position is:');
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+    
+    document.getElementById("info").innerHTML =`Latitude : ${crd.latitude} Longitude: ${crd.longitude} max: ${crd.longitude + 0.5} ` ;
+  
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+*/
